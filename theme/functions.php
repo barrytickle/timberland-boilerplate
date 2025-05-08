25410be6bb81cb6b6943f2158fe27825cc9fbf26
Barry Tickle <barry.tickle12@gmail.com>
2025-05-08 18:19:12 +0100

    Removed portfolio code from boilerplate

--- theme/functions.php
+++ theme/functions.php
@@ -198,6 +198,5 @@ function acf_should_wrap_innerblocks( $wrap, $name ) {
 add_filter( 'acf/blocks/wrap_frontend_innerblocks', 'acf_should_wrap_innerblocks', 10, 2 );
 
 add_filter('timber/twig', function ($twig) {
-	$twig->addFunction(new TwigFunction('logo_split', 'logo_split'));
 	return $twig;
 });
